d3b752ec19ba9a7d91d1200cfbe95fa1157744da
flynn31 <minato20010119@gmail.com>
2022-05-16 18:54:03 +0300

    This is a Product Data Management for sites

---
+++ src/app/products.ts
@@ -0,0 +1,64 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Phone S',
+    price: 291,
+    description: ''
+  },
+  {
+    id: 4,
+    name: 'Photandard',
+    price: 29,
+    description: ''
+  },
+  {
+    id: 5,
+    name: 'rd',
+    price: 699,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 6,
+    name: 'sdfasdf',
+    price: 239,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 7,
+    name: 'Phd',
+    price: 555,
+    description: ''
+  },
+  {
+    id: 8,
+    name: 'Phone number',
+    price: 850,
+    description: 'A large phone with one of the best screens'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
